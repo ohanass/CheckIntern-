52c21e4ae278145482a5f636eb38c3817d39769f
ArturMa1a <152440126+ArturMa1a@users.noreply.github.com>
2023-12-01 16:39:06 -0300

    Update vaga.hpp
    Inserção de comentários

--- Versao1/include/vaga.hpp
+++ Versao1/include/vaga.hpp
@@ -13,13 +13,40 @@ class Vaga{
     std::vector<std::string> _beneficios;
     
     public:
+
+    /// @brief Construtor padrão da classe "Vaga"
     Vaga();
-    Vaga(float salario, std::vector<std::string> requisitos, std::vector<std::string> beneficios) 
+    
+    /// @brief Contrutor da classe "Vaga" com parâmetros
+    /// @param salario Salário atualizado da vaga
+    /// @param requisitos Lista de requisitos atualizada da vaga
+    /// @param beneficios Lista de benefícios atualizada da vaga
+    Vaga(float salario, std::vector<std::string> requisitos, std::vector<std::string> beneficios)
     : _salario(salario), _requisitos(requisitos),  _beneficios(beneficios) {};
+    
+    /// @brief Obtem o salário de uma vaga específica
+    /// @return Salário da vaga
     float get_salario();
-    std::vector<std::string> get_requisitos(void);
-    std::vector<std::string> get_beneficios(void);
+    
+    /// @brief Obtem a lista de requisitos referentes a uma vaga específica
+    /// @return Lista de requisitos da vaga
+    std::vector<std::string> get_requisitos();
+    
+    /// @brief Obtem a lista de benefícios fornecidos por uma vaga específica
+    /// @return Lista de benefícios oferecidos pela vaga
+    std::vector<std::string> get_beneficios();
+    
+    /// @brief Atualiza uma vaga através de paramêtros novos
+    /// @param salario Salário atualizado da vaga
+    /// @param requisitos Lista de requisitos atualizada da vaga
+    /// @param beneficios Lista de benefícios atualizada da vaga
     void atualizar_vaga(float salario, std::vector<std::string> requisitos, std::vector<std::string> beneficios);
+    
+    /// @brief Atualiza uma vaga com os valores de outra vaga
+    /// @param outro Ponteiro que aponta para a vaga que terá seus atributos
+    ///              copiados para a vaga a qual qeuremos atualizar
     void atualizar_vaga(Vaga* outro);
+    
+    /// @brief Exibe informações de uma vaga
     void mostrar_vaga();
 };
