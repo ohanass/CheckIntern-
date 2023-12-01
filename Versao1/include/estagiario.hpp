b1790fe83e5cdb20e166aee2dc062dd3cb90a025
ArturMa1a <152440126+ArturMa1a@users.noreply.github.com>
2023-12-01 16:51:51 -0300

    Update estagiario.hpp
    Inserção de comentários

--- Versao1/include/estagiario.hpp
+++ Versao1/include/estagiario.hpp
@@ -11,11 +11,30 @@ class Estagiario{
     Vaga _curriculo;
 
     public:
+
+    /// @brief Contrutor da classe "Estagiario" com parâmetros
+    /// @param nome Nome do estagiário
+    /// @param senha Senha da conta do estagiário
     Estagiario(std::string nome, std::string senha);
+
+    /// @brief Atualiza o currículo com base nas informações de outro currículo
+    /// @param curriculo Ponteiro que aponta para o currículo que terá seus atributos
+    ///                           copiados para o currículo o qual queremos atualizar
     void atualizar_curriculo(Vaga* curriculo);
+
+    /// @brief Altera a senha do usuário
+    /// @param senha Nova senha da conta do usuário
     void alterar_senha(std::string senha);
+
+    /// @brief Exibe informações do currículo do usuário
     void mostrar_curriculo();
+
+    /// @return O nome do estagiário
     std::string get_nome();
+
+    /// @return A senha do usuário
     std::string get_senha();
+
+    /// @return Currículo de um estagiário
     Vaga get_curriculo();
 };
