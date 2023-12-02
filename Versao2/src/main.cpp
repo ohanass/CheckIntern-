79343dd1aef53c118c1d3172f662d20aca798d8f
ohanass <147887836+ohanass@users.noreply.github.com>
2023-12-02 16:45:21 -0300

    Update main.cpp

--- Versao2/src/main.cpp
+++ Versao2/src/main.cpp
@@ -4,14 +4,19 @@
 #include <utility>
 #include <map>
 
-///separa uma string completa em um vector de string considerando virgula para marcar a divisão
+//separa a string em um vetor
 void separar_Str(std::vector<std::string>* be){
-    std::string aux;
-    char input;
+    std::string aux;        //variável que armazena a string temporária 
+    char input;             //armazena o caractere lido 
     do {
-        input = getchar(); // Lê um caractere do console
+        input = getchar(); 
+        // leitura do caractere 
         if ((input == ','&&!(aux.empty()))||(input == '\n'&&!(aux.empty()))) {
-            be->push_back(aux);
+
+            //verifica se encontrou vírgula E que a string não está vazia
+            //OU se foi encontrada uma quebra de linha e a string não está vazia
+            
+            be->push_back(aux);        //adiciona a string ao vetor 
             aux.clear();
         } else if (input != '\n'&&!(input==' '&&aux.empty())&&input != ',') {
             aux+=input;
