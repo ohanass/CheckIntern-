60a9f52d5948bab7ee075491a9bbd6566d6ab067
ArturMa1a <152440126+ArturMa1a@users.noreply.github.com>
2023-12-03 16:25:37 -0300

    Add files via upload
    Terceira versão da implementação das funções de "aluno.hpp"
    DIFERENÇAS
    Pequena alteração ao imprimir o número de telefone do aluno. Antes, o código apenas imprimia o número, agora ele imprime o número usando o formato "xx xxxxx xxxx"

---
+++ Versao3/src/aluno.cpp
@@ -0,0 +1,48 @@
+#include "aluno.hpp"
+
+void Aluno::mostrar_aluno(void){
+    std::cout << "---------------------------" << std::endl;
+    std::cout << "NOME: " << this->_nome << std::endl;
+    std::cout << "CPF: ";
+    for(unsigned int i = 0;i < 9;i += 3){
+        std::cout << this->_cpf.substr(i,2 + i);
+        if(i < 6){
+            std::cout << ".";
+        }
+    }
+    std::cout << "-" << this->_cpf.substr(9,10);
+    std::cout << "IDADE: " << this->_idade << std::endl;
+    std::cout << "ENDERECO: " << this->_endereco << std::endl;
+    std::cout << "CURSO: " << this->_curso << std::endl;
+    std::cout << "PERIODO: " << this->_periodo << std::endl;
+    std::cout << "CONTATO: " << this->_numeroTelefone.substr(0,2) << " " << _numeroTelefone.substr(2,5) << "-" << _numeroTelefone.substr(7,4) << std::endl;
+    std::cout << "---------------------------" << std::endl;
+}
+
+std::string Aluno::get_nome(void){
+    return this->_nome;
+}
+
+std::string Aluno::get_cpf(void){
+    return this->_cpf;
+}
+
+unsigned short int Aluno::get_idade(void){
+    return this->_idade;
+}
+
+std::string Aluno::get_endereco(void){
+    return this->_endereco;
+}
+
+std::string Aluno::get_curso(void){
+    return this->_curso;
+}
+
+unsigned short int Aluno::get_periodo(void){
+    return this->_periodo;
+}
+
+std::string Aluno::get_numeroTelefone(void){
+    return this->_numeroTelefone;
+}
