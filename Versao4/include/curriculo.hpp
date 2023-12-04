005ef45d80fcd81b63a4f07b1bf16c118552a50e
giovannapoli <152791993+giovannapoli@users.noreply.github.com>
2023-12-03 21:21:52 -0300

    Update curriculo.hpp
    Não houve alteração entre as versões.

--- Versao4/include/curriculo.hpp
+++ Versao4/include/curriculo.hpp
@@ -8,21 +8,51 @@
 
 class Curriculo{
     private:
-    float _salario;
-    std::vector<std::string> _areasInteresse;
-    std::vector<std::string> _aptidoes;
-    std::vector<std::string> _beneficios;
+    float _salario; 
+    std::vector<std::string> _areasInteresse; 
+    std::vector<std::string> _aptidoes; 
+    std::vector<std::string> _beneficios; 
     
     public:
     Curriculo() : _salario(0) {};
-    Curriculo(float salario,std::vector<std::string> areasInteresse, std::vector<std::string> aptidoes, std::vector<std::string> beneficios) : 
+
+    // @brief Construtor da classe Curriculo que permite inicializar todos os atributos.
+    // @param salario Salário associado ao currículo.
+    // @param areasInteresse Vetor de áreas de interesse do estagiário.
+    // @param aptidoes Vetor de aptidões do estagiário.
+    // @param beneficios Vetor de benefícios.
+    // @return Salário associado ao currículo.
+    Curriculo(float salario, std::vector<std::string> areasInteresse, std::vector<std::string> aptidoes, std::vector<std::string> beneficios) : 
     _salario(salario), _areasInteresse(areasInteresse), _aptidoes(aptidoes),  _beneficios(beneficios) {};
+
+    // @brief Obter o salário associado ao currículo.
+    // @return Salário associado ao currículo.
     float get_salario(void);
+
+    // @brief Obter as áreas de interesse do estagiário.
+    // @return Vetor de áreas de interesse.
     std::vector<std::string> get_areasInteresse(void);
+
+    // @brief Obter as aptidões do estagiário.
+    // @return Vetor de aptidões.
     std::vector<std::string> get_aptidoes(void);
+
+    // @brief Obter os benefícios oferecidos pelo estagiário.
+    // @return Vetor de benefícios.
     std::vector<std::string> get_beneficios(void);
-    void atualizar_curriculo(float salario,std::vector<std::string> areasInteresse, std::vector<std::string> requisitos, std::vector<std::string> beneficios);
+
+    // @brief Atualizar os atributos do currículo.
+    // @param salario Novo salário associado ao currículo.
+    // @param areasInteresse Novo vetor de áreas de interesse.
+    // @param requisitos Novo vetor de requisitos (aptidões).
+    // @param beneficios Novo vetor de benefícios.
+    void atualizar_curriculo(float salario, std::vector<std::string> areasInteresse, std::vector<std::string> requisitos, std::vector<std::string> beneficios);
+
+    // @brief Atualizar o currículo com base em outro currículo.
+    // @param outro Ponteiro para outro currículo a ser usado como base para a atualização.
     void atualizar_curriculo(Curriculo* outro);   
+
+    // @brief Mostra as informações do currículo.
     void mostrar_curriculo(void);
 }; 
 #endif
