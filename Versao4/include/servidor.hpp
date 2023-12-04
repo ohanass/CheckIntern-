c84856fb7b9e9af2b7daabf7554e1d941175c180
ohanass <147887836+ohanass@users.noreply.github.com>
2023-12-03 22:43:47 -0300

    Add files via upload

---
+++ Versao4/include/servidor.hpp
@@ -0,0 +1,27 @@
+#ifndef SERVIDOR_HPP
+#define SERVIDOR_HPP
+
+#include <string>
+#include <iostream>
+#include "usuario.hpp"
+
+class Servidor : public  Usuario{
+    private:
+    std::string _nome;
+    std::string _cpf;   
+    std::string _endereco; 
+    std::string _numeroTelefone; 
+    std::string _cadastroInstituicao;
+    public:
+    Servidor(std::string nome, std::string cpf, std::string endereco,std::string numeroTelefone, 
+    std::string cadastroInstituicao, std::string email, std::string login, std::string senha) :
+    _nome(nome), _cpf(cpf), _endereco(endereco), _numeroTelefone(numeroTelefone), _cadastroInstituicao(cadastroInstituicao),
+    Usuario(email, login, senha) {};
+    void mostrar_servidor(void);
+    std::string get_nome(void);
+    std::string get_cpf(void);
+    std::string get_endereco(void);
+    std::string get_numeroTelefone(void);
+    std::string get_cadastroInstituicao(void);
+};
+#endif
